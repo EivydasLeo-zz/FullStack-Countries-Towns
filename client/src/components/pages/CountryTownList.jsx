afd28d55264f13f653290883f347b9f1294509ee
Your Name <you@example.com>
2021-07-21 12:05:45 +0300

    2 new components in pages folder

---
+++ client/src/components/pages/CountryTownList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CountryTownList extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="countryTownListContainer">
+        <h1>countryTownList</h1>
+      </div>
+    );
+  }
+}
+
+export default CountryTownList;
---
+++ client/src/components/pages/NewCountryTown.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class CreateNewCountryOrTown extends Component {
+  state = {};
+  render() {
+    return <div className="createNewCountryOrTown__Container"></div>;
+  }
+}
+
+export default CreateNewCountryOrTown;
