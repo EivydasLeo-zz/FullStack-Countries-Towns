c9d9bf427f4f66891cdc4f3b089c74f31d29310a
Your Name <you@example.com>
2021-07-21 10:04:25 +0300

    boostrap installed, navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
   state = {};
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
   }
 }
 
