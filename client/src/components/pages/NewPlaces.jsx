4339e01176a9bc1c70ae9345b5c6889668772929
Your Name <you@example.com>
2021-07-21 18:23:56 +0300

    countryTown edited to NewPlaces

---
+++ client/src/components/pages/NewPlaces.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class NewPlaces extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="NewPlaces__Container">
+        <form className="row row-cols-lg-auto g-3 align-items-center">
+          <div className="col-12">
+            <label className="visually-hsidden">Title</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Title" />
+            </div>
+            <label className="visually-hsidden">Continent</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Continent" />
+            </div>
+            <label className="visually-hsidden">Population</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Population" />
+            </div>
+          </div>
+          <div className="col-12">
+            <label className="visually-hidden" htmlFor="inlineFormSelectPref">
+              Preference
+            </label>
+            <select className="form-select" id="inlineFormSelectPref">
+              <option selected>Choose...</option>
+              <option value={1}>Country</option>
+              <option value={2}>Town</option>
+            </select>
+          </div>
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NewPlaces;
---
+++ client/src/components/pages/NewPlacesList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NewPlacesList extends Component {
+  state = {};
+  render() {
+    //reiktu isimportinti axios,
+    return (
+      <div className="NewPlacesListContainer">
+        <h1>NewPlacesList</h1>
+      </div>
+    );
+  }
+}
+
+export default NewPlacesList;
